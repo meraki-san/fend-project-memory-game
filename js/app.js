a149cbb6e1c9fa7fb13b26f0f0712a20a5515aad
meraki-san <lucastratton@gmail.com>
2018-05-20 17:30:57 +0200

    Delete extra semicolon and replace "var" with "let" or "const"

--- js/app.js
+++ js/app.js
@@ -1,57 +1,70 @@
 // Variables
-// All cards
-var deck = $(".deck");
 // Single card
-var card = $(".card");
+const card = $(".card");
 // Create a list from card
-var cards = jQuery.makeArray(card);
+let cards = jQuery.makeArray(card);
 // Player moves
-var moves = 0;
-// Rating stars
-var stars = $(".stars").html();
+let moves = 0;
 // Array of opened cards
-var openedCards = [];
+let openedCards = [];
 // Array of matched cards
-var matchedCards = [];
+let matchedCards = [];
 // Seconds
-var seconds = $("#seconds").html();
+let seconds = $("#seconds").html();
 // Minutes
-var minutes = $("#minutes").html();
+let minutes = $("#minutes").html();
 // Symbols Array
-var symbols = [
+let symbols = [
   "diamond", "diamond", "paper-plane-o", "paper-plane-o",
   "anchor", "anchor", "bolt", "bolt", "cube", "cube", "leaf", "leaf",
   "bicycle", "bicycle", "bomb", "bomb"
   ];
 
-function createHTML() {
-  symbols = shuffle(symbols);
-  for (i = 0; i < symbols.length; i++) {
-    $(".deck").append("<li class='card'><i class='fa fa-" + symbols[i] + "'></i></li>");;
-  };
-}
-
-function removeHTML() {
-  for (i = 0; i < symbols.length; i++) {
-    $(".card").remove();
-  };
-}
-
+/**
+* @description Executes funcions to play the game
+*/
 function startGame() {
   createHTML();
   time();
   displayCards();
   openCards();
   restart();
-};
+}
 
+/**
+* @description Shuffle cards and creates HTML
+*/
+function createHTML() {
+  symbols = shuffle(symbols);
+  // TODO: add shuffled cards to the table
+  for (i = 0; i < symbols.length; i++) {
+    $(".deck").append("<li class='card'><i class='fa fa-" + symbols[i] + "'></i></li>");
+  };
+}
+
+/**
+* @description Remove cards HTML
+*/
+function removeHTML() {
+  // TODO: remove cards from the table
+  for (i = 0; i < symbols.length; i++) {
+    $(".card").remove();
+  };
+}
+
+/**
+* @description Flip cards
+*/
 function displayCards() {
   $(".card").click(function() {
     $(this).toggleClass("open");
     $(this).toggleClass("show");
   });
-};
+}
 
+/**
+* @description Check if two cards are matched or not
+*/
 function openCards() {
   $(".card").click(function() {
     $(this).addClass("avoid-clicks");
@@ -68,8 +81,11 @@ function openCards() {
     };
     win();
   });
-};
+}
 
+/**
+* @description Match cards
+*/
 function matched() {
   $(openedCards[0]).addClass("match").removeClass("open show");
   $(openedCards[1]).addClass("match").removeClass("open show");
@@ -78,6 +94,9 @@ function matched() {
   openedCards = [];
 }
 
+/**
+* @description Unmatch cards
+*/
 function unmatched() {
   $(openedCards[0]).addClass("unmatch");
   $(openedCards[1]).addClass("unmatch");
@@ -92,7 +111,7 @@ function unmatched() {
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -100,44 +119,55 @@ function shuffle(array) {
         temporaryValue = array[currentIndex];
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
-    }
+    };
 
     return array;
 }
 
-var interval;
-var time =  function() {
-  var sec = 0;
-  function pad ( val ) { return val > 9 ? val : "0" + val; }
-  interval = setInterval( function(){
+/**
+* @description Starts game time
+*/
+let interval;
+let time =  function() {
+  let sec = 0;
+  function pad (val) {
+    return val > 9 ? val : "0" + val;
+  };
+  interval = setInterval(function() {
     document.getElementById("seconds").innerHTML=pad(++sec%60);
     document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10));
   }, 1000);
-};
+}
 
+/**
+* @description Give a star rating based on your moves
+*/
 function rating() {
   if (moves === 15) {
     $(".stars li:last-child").remove();
   } else if (moves === 25) {
     $(".stars li:last-child").remove();
-  }
-};
+  };
+}
 
+/**
+* @description If you win, these are "congratulations"!
+*/
 function win() {
   if (matchedCards.length === 16) {
     clearInterval(interval);
     $("#myModal").show();
-    $(".modal-body").html("<p>You made " + moves + " moves</p><p>in " + $("#minutes").html() + ":" + $("#seconds").html() + "</p><p id='rating'>Rating:</p>" + $(".stars").html());
+    $(".modal-body").html('<p>You made ' + moves + ' moves</p><p>in ' + $("#minutes").html() + ':' + $("#seconds").html() + '</p><p id="rating">Rating:</p>' + $(".stars").html());
     // Get the modal
-    var modal = document.getElementById('myModal');
+    const modal = document.getElementById('myModal');
 
     // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+    const span = document.getElementsByClassName("close")[0];
 
     // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
         modal.style.display = "none";
-    }
+    };
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
@@ -146,8 +176,11 @@ function win() {
         };
     };
   };
-};
+}
 
+/**
+* @description Restart the game
+*/
 function restart() {
   $(".restart").click(function() {
     clearInterval(interval);
@@ -156,7 +189,7 @@ function restart() {
     if ($(".stars").children().length === 2) {
       $(".stars").append("<li><i class='fa fa-star'></i></li>");
     } else if ($(".stars").children().length === 1) {
-      $(".stars").append("<li><i class='fa fa-star'></i></li> <li><i class='fa fa-star'></i></li>")
+      $(".stars").append("<li><i class='fa fa-star'></i></li> <li><i class='fa fa-star'></i></li>");
     };
     if ($("#myModal").is(":visible")) {
       $("#myModal").hide();
@@ -165,11 +198,11 @@ function restart() {
     removeHTML();
     startGame();
   });
-};
+}
 
 $(document).ready(function() {
   startGame();
-});
+})
 
 
 /*
